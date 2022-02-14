2c0f70015c88a64baca437663354a8c5d11b2248
MBudak <lood2134@gmail.com>
2022-02-14 15:09:33 +0300

    Add Booking

--- Flightplan/obj/Debug/net6.0/Flightplan.AssemblyInfo.cs
+++ Flightplan/obj/Debug/net6.0/Flightplan.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("a9885d7a-d271-46af-b6a0-6edf75f532ed")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flightplan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
--- Flightplan/wwwroot/js/site.js
+++ Flightplan/wwwroot/js/site.js
@@ -29,7 +29,7 @@ function getCoordHTML(obj) {
         '<div class="toWhere">' + (obj.toWhere || "") + '</div>' + '<br>' +
         '<div class="planeId">' + '<div class="text">FLIGHT NO</div>' + (obj.planeId || "") + '</div>'+
         '</div>' +
-        '<button onclick="getPlacesOfPlane(' + obj.id + ')">Click me</button>' +
+        '<button class="Getplaces" onclick="getPlacesOfPlane(' + obj.id + ')">Book</button>' +
         '<div id="hr"><hr></div>' +
         '</div>'
     );
@@ -37,12 +37,12 @@ function getCoordHTML(obj) {
 function getPlacesOfPlane(id) {
     fetch(`${uri}/${url}/${id}`)
         .then(response => response.json())
-        .then(pasangers => _displayPassengers(pasangers))
+        .then(pasangers => _displayPassengers(pasangers,id))
         .catch(error => console.error('Unable to get items.', error));
     
 }
-function _displayPassengers(pasangers) {
-    document.getElementById('Booking').innerHTML = '';
+function _displayPassengers(pasangers, id) {
+    clearArea();
     let seats = [];
     let places = [];
     pasangers.forEach(item => {
@@ -58,19 +58,58 @@ function _displayPassengers(pasangers) {
             places.push('')
         }
     }
+    document.getElementById('ConteinerOfplanes').insertAdjacentHTML('beforeEnd', getForm(id));
     for (var i = 1; i < 37;i++) {
         
-        document.getElementById('Booking').insertAdjacentHTML('beforeEnd', getHTML(places[i - 1], i));
+        document.getElementById('seats').insertAdjacentHTML('beforeEnd', getHTML(places[i - 1], i));
         if (i % 4 == 0) {
-            document.getElementById('Booking').insertAdjacentHTML('beforeEnd', getHTMLbr());
+            document.getElementById('seats').insertAdjacentHTML('beforeEnd', getHTMLbr());
         }
     }
+    document.getElementById('Booking').insertAdjacentHTML('beforeEnd', getbutton());
+}
+function getForm(id) {
+    return (
+        '<form id="Booking" action="javascript:void(0);" method="POST" onsubmit="addItem('+id+')">'+
+        '<div id="seats"></div>'+
+        '</form>'
+    )
+}
+function getbutton() {
+    return (
+        '<div id="formNew">' +
+        '<input type="text" id="PassportId" placeholder="Passport">' +'<br>'+
+        '<input type="submit" value="Book">' +
+        '</div >'
+        )
 }
 function getHTML(places,val) {
     return (
-        '<input type="checkbox" value="'+val+'" ' + places + '/>'
+        '<input type="radio" class="regular-checkbox" name="Seats" value="'+val+'" ' + places + '/>'
         )
 }
 function getHTMLbr() {
     return('<br>')
+}
+function clearArea() {
+    document.getElementById('ConteinerOfplanes').innerHTML = '';
+}
+function addItem(id) {
+    const addPassportTextbox = document.getElementById('PassportId');
+    const seats = document.querySelector('input[name="Seats"]:checked').value;
+    console.log(id);
+    const item = {
+        planeId: id,
+        place: seats,
+        pasportId: addPassportTextbox.value.trim()
+    };
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+    })
+    clearArea();
+    getPlacesOfPlane(id);
 }
